166416cd22c330aa924cc7962688f7b4316ce8a5
Elysia Tegart <etegart@ualberta.ca>
2024-03-05 13:48:28 +0100

    FEAT: Created the User model

---
+++ src/models/User.js
@@ -0,0 +1,39 @@
+import { Schema } from "mongoose";
+
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: false
+        }
+    },
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        }
+    },
+    {
+        password: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        role: {
+            type: String,
+            enum: ["user", "admin", "super_admin"],
+            default: "user"
+        }
+    },
+    {
+        timestamps: true,
+        versionkey: false
+    }
+)
+
+const User = model('User', userSchema)
+
+export default User
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,9 +3,8 @@ import { login, register } from "../controllers/auth.controller.js";
 
 const router = Router();
 
-router.post('/api/auth/register', register)
+router.post('/register', register)
 router.post('/api/auth/login', login)
-
 
 export default router
 
