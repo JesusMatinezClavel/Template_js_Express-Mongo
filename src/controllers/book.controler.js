12b8c40ea8c73a0e27e903bbf1247fb8a3fbf809
JesusMartinezClavel <jmcvalles@gmail.com>
2024-03-07 20:57:22 +0100

    FEAT: Created -> function: getAllBooks (and tested its connection) | UPDATE: Updated ->funcion: createBook (to include the author due to a spelling error)

--- src/controllers/book.controler.js
+++ src/controllers/book.controler.js
@@ -6,11 +6,25 @@ export const createBook = async (req, res) => {
     try {
         const { title, description, author } = req.body
 
-        if(!title||!description){
+        if (!title || !description) {
             return res.status(200).json({
                 success: false,
                 message: `title or description invalid!`,
-            }) 
+            })
+        }
+        
+        const book = await Book.findOne(
+            {
+                title: title,
+                author: author
+            }
+        )
+
+        if (book) {
+            return res.status(200).json({
+                success: false,
+                message: `Book already exists!`,
+            })
         }
 
         const newBook = await Book.create(
@@ -21,25 +35,30 @@ export const createBook = async (req, res) => {
             }
         )
 
-        const book = await Book.findOne(
-            {
-                title: title,
-                author: author
-            }
-        )
 
-        if(book){
-            return res.status(200).json({
-                success: false,
-                message: `Book already exists!`,
-            }) 
-        }
+        res.status(200).json({
+            success: true,
+            message: `New book created!`,
+            Results: newBook
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: `server not responsive`,
+            error: error.message
+        })
+    }
+}
 
-            res.status(200).json({
-                success: true,
-                message: `New book created!`,
-                Results: newBook
-            })
+export const getAllBooks = async (req, res) => {
+    try {
+        const books = await Book.find()
+
+        res.status(200).json({
+            success: true,
+            message: `New book created!`,
+            Results: books
+        })
     } catch (error) {
         res.status(500).json({
             success: false,
--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createBook } from "../controllers/book.controler.js";
+import { createBook, getAllBooks } from "../controllers/book.controler.js";
 
 const router = Router();
 
 router.post('/',createBook)
+router.get('/', getAllBooks)
 
 export default router
 
