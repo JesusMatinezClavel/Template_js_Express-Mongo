1aea5d390d951d0023b16373d908d96471ae08e6
JesusMartinezClavel <jmcvalles@gmail.com>
2024-03-07 20:47:42 +0100

    FEAT: Created -> function: createBook (and tested its connection)

--- src/controllers/book.controler.js
+++ src/controllers/book.controler.js
@@ -1,17 +1,50 @@
+import Book from "../models/Books.js"
 
 
 
-export const createBook = async (req, res) => { 
+export const createBook = async (req, res) => {
     try {
-        res.status(200).json({
-            success: false,
-            message:`New book created!`,
-        })
+        const { title, description, author } = req.body
+
+        if(!title||!description){
+            return res.status(200).json({
+                success: false,
+                message: `title or description invalid!`,
+            }) 
+        }
+
+        const newBook = await Book.create(
+            {
+                title,
+                description,
+                author
+            }
+        )
+
+        const book = await Book.findOne(
+            {
+                title: title,
+                author: author
+            }
+        )
+
+        if(book){
+            return res.status(200).json({
+                success: false,
+                message: `Book already exists!`,
+            }) 
+        }
+
+            res.status(200).json({
+                success: true,
+                message: `New book created!`,
+                Results: newBook
+            })
     } catch (error) {
         res.status(500).json({
             success: false,
-            message:`server not responsive`,
+            message: `server not responsive`,
             error: error.message
         })
-    }    
+    }
 }
